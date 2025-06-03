d1467947945d7c77c466ee92a056d30611f768d5
obajejosh <obaje.josh@gmail.com>
2025-06-03 15:35:50 +0100

    implement initial file and folder structure

---
+++ assets/assets.js
@@ -0,0 +1,139 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import code_icon_dark from './code-icon-dark.png';
+import edu_icon from './edu-icon.png';
+import edu_icon_dark from './edu-icon-dark.png';
+import project_icon from './project-icon.png';
+import project_icon_dark from './project-icon-dark.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import python from './python.png';
+import sql from './sql.png';
+import Csharp from './Csharp.png';
+import tableau from './tableau.png';
+import ml_icon from './ml.png';
+
+
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import joshlogo from './joshlogo.png';
+import joshlogo_dark from './joshlogo_dark.png';
+import database_icon from './database.png';
+import logo_dark from './logo_dark.png';
+import mail_icon from './mail_icon.png';
+import mail_icon_dark from './mail_icon_dark.png';
+import profile_img from './profile-img.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import arrow_icon_dark from './arrow-icon-dark.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import data_analytics_icon from './data-analytics.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+
+export const assets = {
+    user_image,
+    code_icon,
+    code_icon_dark,
+    edu_icon,
+    edu_icon_dark,
+    project_icon,
+    project_icon_dark,
+    vscode,
+    firebase,
+    figma,
+    //Added
+    python,
+    sql,
+    Csharp,
+    tableau,
+    database_icon,
+    //End
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    joshlogo,
+    joshlogo_dark,
+    logo_dark,
+    mail_icon,
+    mail_icon_dark,
+    profile_img,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    arrow_icon_dark,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    ml_icon,
+    data_analytics_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+    right_arrow_bold_dark
+};
+
+export const workData = [
+    {
+        title: 'Fullstack Software Development',
+        description: 'Software Dev',
+        bgImage: '/work-1.png',
+    },
+    {
+        title: 'Mobile App Development',
+        description: 'Mobile App',
+        bgImage: '/work-2.png',
+    },
+    {
+        title: 'Data Analsysis & Reporting',
+        description: 'Reporting',
+        bgImage: '/work-3.png',
+    },
+    {
+        title: 'Machine Learning Projects',
+        description: 'ML Models',
+        bgImage: '/work-4.png',
+    },
+]
+
+export const serviceData = [
+    { icon: assets.web_icon, title: 'Desktop & Web Development', description: 'Desktop & Web application development involves creating applications that run on computers and on the Internet....', link: 'https://www.upwork.com/resources/desktop-application-development' },
+    { icon: assets.data_analytics_icon, title: 'Data Analytics & Reporting', description: 'Reporting is the process of gathering and presenting data in a structured format such as graphs and tables ....', link: 'https://www.tableau.com/analytics/what-is-data-analytics' },
+    { icon: assets.database_icon, title: 'Database Design & Deployment', description: 'A collection of tasks involving various steps taken to implement a database.A good database design is important. It helps you get the right information....', link: 'https://www.geeksforgeeks.org/database-design-in-dbms/' },
+    { icon: assets.ml_icon, title: 'Machine Learning Modeling', description: 'A model of machine learning is a set of programs that can be used to find the pattern and make a decision from an unseen dataset. These days NLP (Natural language Processing) ....', link: 'https://www.geeksforgeeks.org/machine-learning-models/' },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'JavaScript, MERN Stack, Next Js, SQL, C#, Python' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Sc. & M.Sc. in Computer Science' },
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+];
+
+export const toolsData = [
+    
+   
+    assets.vscode, assets.python, assets.mongodb, assets.sql,assets.Csharp,assets.git,assets.tableau
+];
